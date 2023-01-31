4d71ba75752fae644f3136b9fd8d568d95e0e680
funny-family <maksvadik228@gmail.com>
2023-01-31 20:58:10 +0700

    add empty linter configs to each package

---
+++ packages/utils/.eslintrc.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef { import("eslint").Linter.Config } EsLintConfig
+ */
+
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+/** @type {EsLintConfig} */
+module.exports = {};
